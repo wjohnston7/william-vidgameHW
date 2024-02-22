12fce884047a35c4b1ab1786c75cc06fd15e78fe
wjohnston7 <157061764+wjohnston7@users.noreply.github.com>
2024-02-22 15:40:08 -0500

    unknown

--- AnimatedSprite2D.gd
+++ AnimatedSprite2D.gd
@@ -1,17 +1,13 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move()#position.x = position.x -3
-	#print("CHOMP")
-	
-
-func move():
-	if Input.is_action_pressed("jump"):
-		position.y -=1.
+	if Input.is_action_pressed("ui_right")
+		position.x +=1.
+		play("run")
